e16ae9692064279161f0e0129856c97a99764a95
My Name <zuberkhan0808@gmail.com>
2022-05-30 12:13:16 +0530

    make comment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import Batches from './components/Batches';
 import Footer from './components/Footer';
 import './App.css'
 
+
+// add coment
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
